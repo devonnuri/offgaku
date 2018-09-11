ae57d169c4ac54bbc54bdd1dc727a199033869a7
devonnuri <devonnuri@gmail.com>
2018-09-11 17:49:16 +0900

    Add font 'Roboto'

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+        <div className="background" />
         <TopBar />
         <PlaylistBox />
         <LyricsBox />
